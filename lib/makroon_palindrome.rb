9654c19e61f5f8aa4a677f6257a44b2b13dc4ccd
Michael Kroon <kroonology@gmail.com>
2022-11-16 22:16:00 +0000

    Finish working and refactored palindrome method

--- lib/makroon_palindrome.rb
+++ lib/makroon_palindrome.rb
@@ -1,8 +1,15 @@
-# frozen_string_literal: true
+require "makroon_palindrome/version"
 
-require_relative "makroon_palindrome/version"
+class String
+	# returns true for a palindrome, false otherwise
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
 
-module MakroonPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+	private
+
+	# returns content for palindrome testing
+	def processed_content
+		scan(/[a-z]/i).join.downcase
+	end
 end
